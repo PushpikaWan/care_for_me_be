3cbaac9356a8a27ca6ec3a2c54325c65a311eb4b
Pushpika Wanniachchi <pushpika@grubtech.com>
2022-10-29 13:15:07 +0530

    feat: add local run support without verify

--- authorization/auth-helper.js
+++ authorization/auth-helper.js
@@ -4,7 +4,6 @@ const {MONGODB_NAME} = require("../util/env-variables");
 module.exports.authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 //todo - remove this later... by pass auth in local call to dev testing
-  console.log('cameeee', MONGODB_NAME, req.headers.host)
   if (MONGODB_NAME === 'CARE_FOR_ME_DEV') {
     return next();
   }
