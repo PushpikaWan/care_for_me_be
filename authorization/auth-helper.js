ea6e5546dc22139024355f348e0aa1d0d6c0c87b
Pushpika Wanniachchi <pushpika@grubtech.com>
2022-10-29 13:14:50 +0530

    feat: add local run support without verify

--- authorization/auth-helper.js
+++ authorization/auth-helper.js
@@ -1,8 +1,13 @@
 const admin = require("firebase-admin");
+const {MONGODB_NAME} = require("../util/env-variables");
 
 module.exports.authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+//todo - remove this later... by pass auth in local call to dev testing
+  console.log('cameeee', MONGODB_NAME, req.headers.host)
+  if (MONGODB_NAME === 'CARE_FOR_ME_DEV') {
+    return next();
+  }
   if (authHeader) {
     const idToken = authHeader.split(" ")[1];
     admin
